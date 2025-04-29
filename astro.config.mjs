d1b29a2341e7c9d02b1c43c25fd5114573667ef3
John Alex Jacob <johnalexjacob@gmail.com>
2025-04-29 12:46:03 +0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Inter",
+        cssVariable: "--font-display",
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
